816f7ec84f6ec77b5795a2ff0d591783b4adb4e4
Douglas <douglasdanielx@gmail.com>
2025-06-23 05:41:42 -0300

    feat: adiciona o html do componente 'Topo'

---
+++ src/components/Topo/Topo.jsx
@@ -0,0 +1,11 @@
+import imgLogo from "@/../public/logo.png"
+
+export default function Topo(){
+    return (
+        <header>
+            <img src={imgLogo.src} alt="" />
+            <button>Icone</button>
+        </header>
+    );
+
+}
