e99d5dda8acd7449121813aa511fef6418bcf107
Douglas <douglasdanielx@gmail.com>
2025-06-27 13:53:56 -0300

    style: adiciona estilo ao 'header'

--- src/components/Topo/Topo.jsx
+++ src/components/Topo/Topo.jsx
@@ -1,10 +1,11 @@
 import imgLogo from "@/../public/logo.png"
+import estilo from "./Topo.module.css"
 
 export default function Topo(){
     return (
-        <header>
-            <img src={imgLogo.src} alt="" />
-            <button>Icone</button>
+        <header id={estilo.header}>
+            <img className={estilo.img} src={imgLogo.src} alt="Logotipo da loja, um 'M'" />
+            <button className={estilo.icone}>Icone</button>
         </header>
     );
 
