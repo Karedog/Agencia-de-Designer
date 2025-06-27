a0050855acccafd91e7eff6da112ba6755920e61
Douglas <douglasdanielx@gmail.com>
2025-06-27 14:15:15 -0300

    style: adiciona estilo a 'secao banner'

--- src/components/SecaoBanner/SecaoBanner.jsx
+++ src/components/SecaoBanner/SecaoBanner.jsx
@@ -1,12 +1,10 @@
 import estilo from "./SecaoBanner.module.css"
-import Image from "next/image";
 import imagemBanner from "../../../public/banner.jpg"
 
 export default function SecaoBanner(){
     return (
         <section id={estilo.secao_banner}>
             <div className={estilo.caixa_img_banner}>
-                <Image className={estilo.imagem_banner}src={imagemBanner.src} alt="Banner" width={500} height={500}/>
             </div>
             <div className={estilo.caixa_texto_banner}>
                 <p>BRANDING / UI / UX / TECNOLOGIA</p>
