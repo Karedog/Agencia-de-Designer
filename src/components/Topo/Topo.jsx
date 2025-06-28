5c1bead8cb9818618cce5b83157c96d31f4f78eb
Douglas <douglasdanielx@gmail.com>
2025-06-28 10:34:47 -0300

    fix: adiciona uma imagem para o icone do botão no 'header'

--- src/components/Topo/Topo.jsx
+++ src/components/Topo/Topo.jsx
@@ -1,11 +1,13 @@
 import imgLogo from "@/../public/logo.png"
+import iconeMoon from "@/../public/moon.png"
+import iconeSun from "@/../public/sun.png"
 import estilo from "./Topo.module.css"
 
 export default function Topo(){
     return (
         <header id={estilo.header}>
             <img className={estilo.img} src={imgLogo.src} alt="Logotipo da loja, um 'M'" />
-            <button className={estilo.icone}>Icone</button>
+            <button className={estilo.icone}><img src={iconeMoon.src} alt="icone de lua" className={estilo.botao_iconeLua} /></button>
         </header>
     );
 
