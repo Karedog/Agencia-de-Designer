57f7bcc820520893c665e85bd55c172fed49a2dd
Douglas <douglasdanielx@gmail.com>
2025-06-28 09:53:54 -0300

    style: adiciona css ao 'Card, atualiza a paleta de cores geral'

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,9 +3,11 @@ import estilo from "./Card.module.css"
 export default function Card(props){
     return (
         <div className={estilo.card}>
-            <p className={estilo.card_data}>{props.data}</p>
-            <h3 className={estilo.card_titulo}>{props.titulo}</h3>
-            <p className={estilo.card_empresa}>{props.empresa}</p>
+            <div className={estilo.card_dados}>
+                <p className={estilo.card_data}>{props.data}</p>
+                <h3 className={estilo.card_titulo}>{props.titulo}</h3>
+                <small className={estilo.card_empresa}>{props.empresa}</small>
+            </div>
             <p className={estilo.card_info}>{props.info}</p>
         </div>
     );
