eee32e53377df67a08ca9b3726e60f83497a55b5
Douglas <douglasdanielx@gmail.com>
2025-06-28 09:20:02 -0300

    style: adiciona estilo a 'secao experiencia'

--- src/components/SecaoExperienciaTrabalho/SecaoExperienciaTrabalho.jsx
+++ src/components/SecaoExperienciaTrabalho/SecaoExperienciaTrabalho.jsx
@@ -11,26 +11,26 @@ export default function SecaoExperienciaTrabalho(){
                     Sites e Marketing Digital, nos empenhamos diariamente para entregar
                     resultados que tragam impacto aos nossos clientes.
                 </p>
-                <div className={estilo.caixa_cards}>
-                    <Card 
-                        data="JUNHO 2012 - 2016" 
-                        titulo="Web Designer" 
-                        empresa="Pied Piper StartUp." 
-                        info="Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores"
-                    />
-                    <Card 
-                        data="AGOSTO 2016 - 2019" 
-                        titulo="Product Designer" 
-                        empresa="E Corp." 
-                        info="Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra"
-                    />
-                    <Card 
-                        data="FEVEREIRO 2019 - 2021" 
-                        titulo="Digital Consulting" 
-                        empresa="Arasaka Inc." 
-                        info="Consultoria em estratégias digitais para todas as etapas do ciclo do projeto, desde a definição inicial até a execução"
-                    />
-                </div>
+            </div>
+            <div className={estilo.caixa_cards}>
+                <Card 
+                    data="JUNHO 2012 - 2016" 
+                    titulo="Web Designer" 
+                    empresa="Pied Piper StartUp." 
+                    info="Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores"
+                />
+                <Card 
+                    data="AGOSTO 2016 - 2019" 
+                    titulo="Product Designer" 
+                    empresa="E Corp." 
+                    info="Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra"
+                />
+                <Card 
+                    data="FEVEREIRO 2019 - 2021" 
+                    titulo="Digital Consulting" 
+                    empresa="Arasaka Inc." 
+                    info="Consultoria em estratégias digitais para todas as etapas do ciclo do projeto, desde a definição inicial até a execução"
+                />
             </div>
         </section>
     );
